14fbd68c90e2a0fd22b0f40f12d17526644d7a53
AntonZakharo <antonchik2019top@gmail.com>
2025-07-16 11:00:48 +0300

    errors + auto-request

--- module/fetchAndRenderComments.js
+++ module/fetchAndRenderComments.js
@@ -4,10 +4,20 @@ import { updateComments } from './commentsArray.js';
 export function fetchAndRenderComments() {
     return fetch(`https://wedev-api.sky.pro/api/v1/anton-zakharov/comments`)
         .then((response) => {
-            return response.json();
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                if (response.status == 404) {
+                    throw new Error('Сервер сломался, попробуй позже');
+                }
+                throw new Error('Что-то сломалось');
+            }
         })
         .then((data) => {
             updateComments(data.comments);
             renderComments();
+        })
+        .catch((error) => {
+            alert(error);
         });
 }
